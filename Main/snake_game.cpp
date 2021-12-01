51b9c09f1a89ae51acf21c1b953766cc54f71922
Ben Stone <spector.8500@gmail.com>
2021-11-30 21:02:56 -0500

    Scaffolding

--- Main/snake_game.cpp
+++ Main/snake_game.cpp
@@ -1,1 +1,42 @@
-int main() {}
+#include <SFML/Graphics.hpp>
+
+void place_treat_in_random_location() {
+
+}
+
+void process_input() {
+
+}
+
+void load_textures() {
+	// fill texture manager
+}
+
+class TextureManager {
+
+};
+
+class Game {
+	// holds texture manager, member variables across the game
+};
+
+class Player {
+	// process player related input
+};
+
+class Actor {
+	// x, y position
+};
+
+class Snake {
+
+};
+
+class PlayArea {
+	// data
+	// list of actors
+};
+
+int main() {
+	sf::RenderWindow window(sf::VideoMode(800, 600), "danger noodle");
+}
